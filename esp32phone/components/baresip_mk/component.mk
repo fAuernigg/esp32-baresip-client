aa373e747645838c9f36ccfebe39ed04c3596396
Christian Spielberger <c.spielberger@commend.com>
2019-08-03 18:47:37 +0200

    baresip: lib baresip builds without modules

---
+++ esp32phone/components/baresip_mk/component.mk
@@ -0,0 +1,34 @@
+
+COMPONENT_ADD_INCLUDEDIRS := ../baresip/include
+
+COMPONENT_SRCDIRS = \
+../baresip/src  \
+
+CFLAGS	+= -DHAVE_SELECT -DHAVE_SELECT_H
+CFLAGS	+= -DHAVE_INET_NTOP -DHAVE_INET_PTON -DHAVE_PTHREAD -DHAVE_STRERROR_R
+CFLAGS	+= -DHAVE_INTTYPES_H -DHAVE_STDBOOL_H -DHAVE_FORK
+CFLAGS	+= -DHAVE_PWD_H
+CFLAGS  += -DHAVE_SIGNAL -DHAVE_SYS_TIME_H
+CFLAGS  += -DHAVE_UNISTD_H -DHAVE_STRINGS_H
+CFLAGS	+= -DHAVE_ROUTE_LIST
+
+CFLAGS	+= -Wall
+CFLAGS	+= -Wmissing-declarations
+CFLAGS	+= -Wmissing-prototypes
+CFLAGS	+= -Wbad-function-cast
+CFLAGS	+= -Wsign-compare
+CFLAGS	+= -Wnested-externs
+CFLAGS	+= -Wshadow
+CFLAGS	+= -Waggregate-return
+CFLAGS  += -DLINUX -g -std=c99
+CFLAGS  += -Os
+
+CFLAGS	+= -DSHARE_PATH=\"/usr/share/baresip\"
+
+COMPONENT_OBJEXCLUDE = \
+					   ../baresip/src/video.o \
+					   ../baresip/src/vidsrc.o \
+					   ../baresip/src/vidutil.o \
+					   ../baresip/src/vidisp.o \
+					   ../baresip/src/vidcodec.o \
+					   ../baresip/src/vidfilt.o 
