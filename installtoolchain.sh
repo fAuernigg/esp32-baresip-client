c770c332dbb33fdb31ffd487074a7ccf85809db5
mrxa <franz.auernigg@gmail.com>
2019-09-09 21:57:02 +0200

    install cleanup

--- installtoolchain.sh
+++ installtoolchain.sh
@@ -20,8 +20,9 @@ export PATH=$PATH:$spath/xtensa-esp32-elf/bin
 
 
 cd $spath
-git submodule update --init
 if [ $? -ne 0 ] ; then echo "error cd ~/$spath" ; exit 1; fi
+#git submodule update --init
+
 
 cd $spath/esp-idf/
 if [ $? -ne 0 ] ; then echo "error cd esp-idf" ; exit 1; fi
@@ -33,15 +34,14 @@ git checkout release/v3.2
 cd "$spath"
 python -m pip install --user -r esp-idf/requirements.txt
 
-
 cd $spath/esp32phone/components/re/
 if [ $? -ne 0 ] ; then echo "error components/re does not exist" ; exit 1; fi
 
 #git checkout *
 git checkout "esp_v0.5.8"
-
 #patch -p1 -i ../re_mk/patches/0001-fix-build-for-esp32.patch
 #patch -p1 -i ../re_mk/patches/0002-tcp-udp-hmac-apis-fix-multi-defs.patch
+
 
 cd $spath/esp32phone/components/baresip/
 if [ $? -ne 0 ] ; then echo "error components/baresip does not exist" ; exit 1; fi
