d4cf9f10e216aec24cddb4e435c1521e3be6e05b
Franz Auernigg <f.auernigg@commend.com>
2019-09-19 16:57:05 +0200

    mqttSubscribe.sh: support search for all devices with #

--- esp32phone/mqttSubscribe.sh
+++ esp32phone/mqttSubscribe.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 if [ $# -eq 0 ] ; then
-	echo "Error params: specify baresip command in format: \"esp32phone_[MAC]\" cmd arg1 arg2"
-	exit 1
+	topic="#"
+else
+	topic="$1"
 fi
-
-devicename=$1
-shift
-
-topic="#"
 
 x=$(mosquitto_pub --help | grep "help" | tail -n 1)
 if [[ "x$x" == "x" ]] ; then
@@ -23,4 +19,4 @@ mqttport=1883
 mqttuser=esp32phone
 
 
-mosquitto_sub -h $mqttserver -p $mqttport -t "$devicename/$topic"  -q 2 -i MyPcClient -u "esp32phone" -P "$mqttpass" -v
+mosquitto_sub -h $mqttserver -p $mqttport -t "$topic"  -q 2 -i MyPcClient -u "esp32phone" -P "$mqttpass" -v
